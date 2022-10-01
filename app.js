20787559de6f1ce1ce802bb921fe10f164e9542f
Krystian <krystian1wojtowicz@gmail.com>
2022-10-01 16:37:09 +0200

    removing unnecessary code app.js

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ db.on("error", console.error.bind(console, "MongoDB connection error"));
 // func();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
