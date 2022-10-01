c1aa8cc9e267ecc094d340084fd44c6b52dbfcf8
Krystian <krystian1wojtowicz@gmail.com>
2022-10-01 17:16:05 +0200

    test heroku

---
+++ controllers/userController.js
@@ -0,0 +1,6 @@
+// const User = require("../models/user");
+
+// exports.signup_post = (req, res) => {
+//   let user = new User(req.body);
+//   user.save();
+// };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userController = require("../controllers/userController");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
+
+// router.post("/signup", userController.signup_post);
 
 module.exports = router;
