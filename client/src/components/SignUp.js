42ce24a6d91b0ebb3af3f316ec706db75ad6b065
Krystian <krystian1wojtowicz@gmail.com>
2022-10-02 14:10:44 +0200

    error handling

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -16,7 +16,7 @@ const SigUp = () => {
   };
   return (
     <div className="register">
-      <h1>Registerrr</h1>
+      <h1>Register</h1>
       <input
         className="inputBox"
         type="text"
