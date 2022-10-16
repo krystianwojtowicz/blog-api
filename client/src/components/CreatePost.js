f40436acd2f24de53efc6363cb9b5e3ffdc5558c
Krystian <krystian1wojtowicz@gmail.com>
2022-10-16 16:00:15 +0200

    move creatingpost to controller

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 
 const CreatePost = () => {
+  const auth = JSON.parse(localStorage.getItem("user")).username;
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  // const [author, setAuthor] = useState("");
+  // schowane nowe
   const handleCreate = async () => {
     console.log(title, content);
     let result = await fetch(
@@ -22,6 +25,13 @@ const CreatePost = () => {
   return (
     <div className="create-post">
       <h1>Create Post</h1>
+      {/* <input
+        className="inputBox"
+        type="text"
+        placeholder="enter title"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      /> */}
       <input
         className="inputBox"
         type="text"
@@ -44,3 +54,56 @@ const CreatePost = () => {
 };
 
 export default CreatePost;
+// import React, { useState } from "react";
+
+// const CreatePost = (props) => {
+//   // const auth = localStorage.getItem("user");
+//   const [formData, setFormData] = useState({
+//     title: props.title || "",
+//     content: props.content || "",
+//   });
+//   // const [title, setTitle] = useState("");
+//   // const [content, setContent] = useState("");
+//   // const [author, setAuthor] = useState("");
+//   const handleCreate = async () => {
+//     console.log(formData.title, formData.content);
+//     let result = await fetch("http://localhost:5000/create-post", {
+//       method: "post",
+//       body: JSON.stringify({ formData }),
+//       headers: {
+//         "Content-Type": "application/json",
+//         authorization: JSON.parse(localStorage.getItem("token")),
+//       },
+//     });
+//     result = await result.json();
+//     console.warn(result);
+//   };
+//   return (
+//     <div className="create-post">
+//       <h1>Create Post</h1>
+//       <input
+//         className="inputBox"
+//         type="text"
+//         placeholder="enter title"
+//         defaultValue={formData.title || ""}
+//         onChange={handleChange}
+//         // value={title}
+//         // onChange={(e) => setTitle(e.target.value)}
+//       />
+//       <textarea
+//         className="inputBox"
+//         maxLength="100"
+//         type="text"
+//         defaultValue={formData.content || ""}
+//         onChange={handleChange}
+//         // value={content}
+//         // onChange={(e) => setContent(e.target.value)}
+//       />
+//       <button className="appButton" type="button" onClick={handleCreate}>
+//         Post
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default CreatePost;
--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const jwt = require("jsonwebtoken");
 
 /* GET posts listing. */
 exports.posts = function (req, res, next) {
@@ -17,33 +18,78 @@ exports.post_detail = (req, res, next) => {
       return res.status(404).json({
         message: "Article not found",
       });
-
-    //   async.parallel(
-    //     {
-    //       genre(callback) {
-    //         Genre.findById(req.params.id).exec(callback);
-    //       },
-
-    //       genre_books(callback) {
-    //         Book.find({ genre: req.params.id }).exec(callback);
-    //       },
-    //     },
-    //     (err, results) => {
-    //       if (err) {
-    //         return next(err);
-    //       }
-    //       if (results.genre == null) {
-    //         // No results.
-    //         const err = new Error("Genre not found");
-    //         err.status = 404;
-    //         return next(err);
-    //       }
     // Successful, so render
     return res.json(post);
-    //   res.render("genre_detail", {
-    //     title: "Genre Detail",
-    //     genre: results.genre,
-    //     genre_books: results.genre_books,
-    //   });
   });
 };
+
+// exports.create_post = (req, res) => {
+//     jwt.verify(req.token, "secretkey", async (err, authData) => {
+//     try {
+//       let post = new Post(req.body);
+//       // let post = new Post({
+//       //   title: req.body.title,
+//       //   content: req.body.content,
+//       //   author: req.body.author,
+//       //   // author: res.locals.currentUser.username,
+//       // });
+//       // post.author = res.locals.currentUser;
+
+//       const savePost = await post.save();
+//       if (savePost) {
+//         return res.status(201).json({
+//           message: "Post created",
+//           post,
+//         });
+//       }
+//       return res.status(500).json({
+//         error: "Error. Try again later",
+//       });
+//     } catch (err) {
+//       return res.status(500).json({
+//         error: err.message,
+//       });
+//     }
+//   });
+// };
+
+exports.create_post = async (req, res, next) => {
+  try {
+    let post = new Post(req.body);
+    // let post = new Post({
+    //   title: req.body.title,
+    //   content: req.body.content,
+    //   author: req.body.author,
+    //   // author: res.locals.currentUser.username,
+    // });
+    // post.author = res.locals.currentUser;
+
+    const savePost = await post.save();
+    if (savePost) {
+      return res.status(201).json({
+        message: "Post created",
+        post,
+      });
+    }
+    return res.status(500).json({
+      error: "Error. Try again later",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message,
+    });
+  }
+};
+
+// exports.create_post = async (req, res, next) => {
+//   try {
+//     await Post.create(req.body);
+//     return res.json({
+//       message: "Post created",
+//     });
+//   } catch (err) {
+//     return res.json({
+//       error: err.message,
+//     });
+//   }
+// };
