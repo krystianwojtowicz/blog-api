d5d4575caa1c088c584ee2e46d9aa9905fcfe999
Krystian <krystian1wojtowicz@gmail.com>
2022-10-19 21:19:43 +0200

    updating and deleting post

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const CreatePost = () => {
+const CreatePost = (props) => {
   const author = JSON.parse(localStorage.getItem("user")).username;
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
@@ -43,7 +43,14 @@ const CreatePost = () => {
         value={content}
         onChange={(e) => setContent(e.target.value)}
       />
-      <button className="appButton" type="button" onClick={handleCreate}>
+      <button
+        className="appButton"
+        type="button"
+        onClick={() => {
+          handleCreate();
+          props.handleGetPosts();
+        }}
+      >
         Post
       </button>
     </div>
@@ -51,56 +58,3 @@ const CreatePost = () => {
 };
 
 export default CreatePost;
-// import React, { useState } from "react";
-
-// const CreatePost = (props) => {
-//   // const auth = localStorage.getItem("user");
-//   const [formData, setFormData] = useState({
-//     title: props.title || "",
-//     content: props.content || "",
-//   });
-//   // const [title, setTitle] = useState("");
-//   // const [content, setContent] = useState("");
-//   // const [author, setAuthor] = useState("");
-//   const handleCreate = async () => {
-//     console.log(formData.title, formData.content);
-//     let result = await fetch("http://localhost:5000/create-post", {
-//       method: "post",
-//       body: JSON.stringify({ formData }),
-//       headers: {
-//         "Content-Type": "application/json",
-//         authorization: JSON.parse(localStorage.getItem("token")),
-//       },
-//     });
-//     result = await result.json();
-//     console.warn(result);
-//   };
-//   return (
-//     <div className="create-post">
-//       <h1>Create Post</h1>
-//       <input
-//         className="inputBox"
-//         type="text"
-//         placeholder="enter title"
-//         defaultValue={formData.title || ""}
-//         onChange={handleChange}
-//         // value={title}
-//         // onChange={(e) => setTitle(e.target.value)}
-//       />
-//       <textarea
-//         className="inputBox"
-//         maxLength="100"
-//         type="text"
-//         defaultValue={formData.content || ""}
-//         onChange={handleChange}
-//         // value={content}
-//         // onChange={(e) => setContent(e.target.value)}
-//       />
-//       <button className="appButton" type="button" onClick={handleCreate}>
-//         Post
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default CreatePost;
--- controllers/postController.js
+++ controllers/postController.js
@@ -12,15 +12,26 @@ exports.posts = function (req, res, next) {
 
 // Display detail page for a specific post.
 exports.post_detail = (req, res, next) => {
-  Post.findById(req.params.id).exec((err, item) => {
+  Post.findById(req.params.id).exec((err, post) => {
     if (err) return next(err);
-    if (!item)
+    if (!post)
       return res.status(404).json({
         message: "Article not found",
       });
     // Successful, so render
     return res.json(post);
+    // res.send(result);
   });
+};
+
+exports.delete_post = async (req, res) => {
+  let result = await Post.deleteOne({ _id: req.params.id });
+  res.send(result);
+};
+
+exports.update_post = async (req, res) => {
+  let result = await Post.updateOne({ _id: req.params.id }, { $set: req.body });
+  res.send(result);
 };
 
 // exports.create_post = (req, res) => {
