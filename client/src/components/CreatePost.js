5689afe4e95ebb8107100e8ce64aa08c5247c2be
Krystian <krystian1wojtowicz@gmail.com>
2022-11-12 09:59:31 +0100

    axios also in create post

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,74 +1,74 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import Axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Axios from "axios";
 
-// const CreatePost = (props) => {
-//   const author = JSON.parse(localStorage.getItem("user")).username;
-//   const [title, setTitle] = useState("");
-//   const [content, setContent] = useState("");
-//   const navigate = useNavigate();
-//   let headers = {
-//     headers: {
-//       "Content-Type": "application/json",
-//       authorization: JSON.parse(localStorage.getItem("token")),
-//     },
-//   };
-//   const handleCreate = (e) => {
-//     e.preventDefault();
-//     console.log(title, content);
-//     // let result = await fetch(
-//     //   "https://blog-api-krystian.herokuapp.com/posts/create-post",
-//     Axios.post(
-//       "http://localhost:5000/posts/create-post",
-//       title,
-//       content,
-//       author,
-//       headers
-//     )
-//       .then((res) => {
-//         console.log(res.data);
-//       })
-//       .catch((err) => {
-//         console.error(err);
-//       });
-//     // result = await result.json();
-//     // console.warn(result);
-//     navigate("/");
-//   };
-//   return (
-//     <div className="create-post">
-//       <div>
-//         <h1>Create Post</h1>
-//         <form
-//           onSubmit={(e) => {
-//             handleCreate(e);
-//             props.handleGetPosts();
-//           }}
-//         >
-//           <input
-//             className="inputBox"
-//             type="text"
-//             placeholder="enter title"
-//             value={title}
-//             onChange={(e) => setTitle(e.target.value)}
-//           />
-//           <textarea
-//             className="inputBox"
-//             maxLength="100"
-//             type="text"
-//             value={content}
-//             onChange={(e) => setContent(e.target.value)}
-//           />
-//           <button className="appButton" type="submit">
-//             Create Post
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
+const CreatePost = (props) => {
+  const author = JSON.parse(localStorage.getItem("user")).username;
+  // console.log(localStorage.getItem("token"));
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const navigate = useNavigate();
+  let headers = {
+    headers: {
+      "Content-Type": "application/json",
+      authorization: localStorage.getItem("token"),
+    },
+  };
+  const handleCreate = (e) => {
+    e.preventDefault();
+    console.log(title, content);
+    const payload = {
+      title: title,
+      content: content,
+      author: author,
+    };
+    // let result = await fetch(
+    //   "https://blog-api-krystian.herokuapp.com/posts/create-post",
+    Axios.post("http://localhost:5000/posts/create-post", payload, headers)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    // result = await result.json();
+    // console.warn(result);
+    navigate("/");
+  };
+  return (
+    <div className="create-post">
+      <div>
+        <h1>Create Post</h1>
+        <form
+          onSubmit={(e) => {
+            handleCreate(e);
+            props.handleGetPosts();
+          }}
+        >
+          <input
+            className="inputBox"
+            type="text"
+            placeholder="enter title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            className="inputBox"
+            maxLength="100"
+            type="text"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <button className="appButton" type="submit">
+            Create Post
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-// export default CreatePost;
+export default CreatePost;
 
 // import { useNavigate } from "react-router-dom";
 // import React, { useState, useEffect } from "react";
@@ -142,67 +142,68 @@
 // };
 // export default SignUp;
 
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Axios from "axios";
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import Axios from "axios";
 
-const CreatePost = (props) => {
-  const author = JSON.parse(localStorage.getItem("user")).username;
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const navigate = useNavigate();
-  const handleCreate = async (e) => {
-    e.preventDefault();
-    console.log(title, content);
+// const CreatePost = (props) => {
+//   const author = JSON.parse(localStorage.getItem("user")).username;
+//   // console.log(localStorage.getItem("token"));
+//   const [title, setTitle] = useState("");
+//   const [content, setContent] = useState("");
+//   const navigate = useNavigate();
+//   const handleCreate = async (e) => {
+//     e.preventDefault();
+//     console.log(title, content);
 
-    let result = await fetch(
-      "https://blog-api-krystian.herokuapp.com/posts/create-post",
-      // let result = Axios.post(
-      //   "https://blog-api-krystian.herokuapp.com/posts/create-post",
-      {
-        method: "post",
-        body: JSON.stringify({ title, content, author }),
-        headers: {
-          "Content-Type": "application/json",
-          authorization: localStorage.getItem("token"),
-        },
-      }
-    );
-    result = await result.json();
-    console.warn(result);
-    navigate("/");
-  };
-  return (
-    <div className="create-post">
-      <div>
-        <h1>Create Post</h1>
-        <form
-          onSubmit={(e) => {
-            handleCreate(e);
-            props.handleGetPosts();
-          }}
-        >
-          <input
-            className="inputBox"
-            type="text"
-            placeholder="enter title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <textarea
-            className="inputBox"
-            maxLength="100"
-            type="text"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-          <button className="appButton" type="submit">
-            Create Post
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-};
+//     let result = await fetch(
+//       "https://blog-api-krystian.herokuapp.com/posts/create-post",
+//       // let result = Axios.post(
+//       //   "https://blog-api-krystian.herokuapp.com/posts/create-post",
+//       {
+//         method: "post",
+//         body: JSON.stringify({ title, content, author }),
+//         headers: {
+//           "Content-Type": "application/json",
+//           authorization: localStorage.getItem("token"),
+//         },
+//       }
+//     );
+//     result = await result.json();
+//     console.warn(result);
+//     navigate("/");
+//   };
+//   return (
+//     <div className="create-post">
+//       <div>
+//         <h1>Create Post</h1>
+//         <form
+//           onSubmit={(e) => {
+//             handleCreate(e);
+//             props.handleGetPosts();
+//           }}
+//         >
+//           <input
+//             className="inputBox"
+//             type="text"
+//             placeholder="enter title"
+//             value={title}
+//             onChange={(e) => setTitle(e.target.value)}
+//           />
+//           <textarea
+//             className="inputBox"
+//             maxLength="100"
+//             type="text"
+//             value={content}
+//             onChange={(e) => setContent(e.target.value)}
+//           />
+//           <button className="appButton" type="submit">
+//             Create Post
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default CreatePost;
+// export default CreatePost;
