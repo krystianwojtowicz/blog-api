ca0ad074ed70b19afe388677d0284d08b244cafb
Krystian <krystian1wojtowicz@gmail.com>
2022-11-27 11:15:16 +0100

    showing date, fixing error in post.js

--- models/post.js
+++ models/post.js
@@ -1,29 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-<<<<<<< HEAD
 const PostSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   author: { type: String, ref: "User" },
+  date: { type: Date, default: new Date() },
   comments: { type: Array, default: [] },
 });
-=======
-const PostSchema = new Schema(
-  {
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    author: { type: String, ref: "User" },
-    date: { type: Date, default: new Date() },
-    // author: { type: Schema.Types.ObjectId, required: true, ref: "User" },
-    comments: { type: Array, default: [] },
-    // published: { type: Boolean },
-    // timestamp: { type: Date },
-    // imgUrl: { type: String },
-    // likes: { type: Array, default: [] },
-  }
-  // { typeKey: "$type" }
-);
->>>>>>> 40ba8d3 (showing date)
 
 module.exports = mongoose.model("Post", PostSchema);
